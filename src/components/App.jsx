b2165a6080a98c63d851b4235a9c7ef3d6985e91
Michael <fidd.michael@gmail.com>
2023-08-15 15:02:18 +0200

    add forms

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template ##
-    </div>
-  );
-};
+import { Component } from "react"
+// import { nanoid } from 'nanoid'
+import { Formik, Field, Form } from 'formik';
+
+export class App extends Component {
+
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  onContactAdd = (newContact) => {
+    console.log(newContact)
+    }
+  
+
+  render(){
+    return (
+    <>
+      <section>
+        <h2>Phonebook</h2>
+        <Formik
+          initialValues={{
+          name: '',
+          tel: '',
+          }}
+
+        onSubmit={(values) => {
+          this.onContactAdd(values)
+        }}
+        >
+          <Form>
+            <label>
+              Name
+              <Field name="name" placeholder="Contact name..." />
+            </label>
+            <label>
+              Tel.
+              <Field name="tel" type = "tel" placeholder="000-000-000" />
+            </label>
+            <button type = "submit">Add contact</button>
+          </Form>
+        </Formik>
+      </section>
+      </>
+)}}
